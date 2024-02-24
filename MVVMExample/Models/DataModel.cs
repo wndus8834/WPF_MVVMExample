123f0233da52f24c22f5d867397d053c6718d667
wndus8834 <wndus8834@naver.com>
2024-02-24 17:35:32 +0900

    .

---
+++ MVVMExample/Models/DataModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace MVVMExample.Models
+{
+	public class DataModel : INotifyPropertyChanged
+	{
+		private double _number;
+
+		public double Number
+		{
+			get { return _number; }
+			set
+			{
+				if (_number != value)
+				{
+					_number = value;
+					OnPropertyChanged(nameof(Number));
+					OnPropertyChanged(nameof(DoubledNumber));
+				}
+			}
+		}
+
+		public double DoubledNumber => Number * 2;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ MVVMExample/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using MVVMExample.Models;
+
+namespace MVVMExample.ViewModels
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		private DataModel _data;
+
+		public MainViewModel()
+		{
+			_data = new DataModel();
+		}
+
+		public DataModel Data
+		{
+			get { return _data; }
+			set
+			{
+				if (_data != value)
+				{
+					_data = value;
+					OnPropertyChanged(nameof(Data));
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
